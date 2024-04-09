6c505cc2c6fde5a1021f7238c4a3f39ac910481a
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-09 04:29:04 +0000

    good night

---
+++ debugger.rb
@@ -0,0 +1,7 @@
+require "pry-byebug"
+
+f= "your lucky number is "
+
+l = rand(100)
+
+pp f + l
--- howdy.rb
+++ howdy.rb
@@ -1,3 +1,8 @@
-intro= "Hello World!"
+require "active_support/all"
+require "pry-byebug"
+
+  intro= "Hello World!"
   message= "I am excited to be doing this"
 pp intro + message
+
+require "./goodbye.rb"
