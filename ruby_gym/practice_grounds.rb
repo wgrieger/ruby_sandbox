3cb93806d0ad12cb33037cd92400070dd8017060
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-10 02:29:57 +0000

    continue

--- ruby_gym/practice_grounds.rb
+++ ruby_gym/practice_grounds.rb
@@ -6,4 +6,24 @@ years = [
 year = years.sample
 # write your program below
 pp year
-if year%
+#parent & step 1
+if 
+  year%4=0
+  #parent
+  then
+#step 2 
+    if year%100=0
+      then 
+      #step3
+      if year%400=0
+          then pp "leap year"
+        else pp "no leap year (from step 3)"
+        end 
+    #step 2 
+      else 
+        pp "leap year (from step 2)"  
+      end 
+#parent
+  else 
+    pp "no leap year"
+  end 
