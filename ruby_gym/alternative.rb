96b9b6e491a78d2da1b9d4b1a13d616b4223657f
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-10 06:06:19 +0000

    workign still

--- ruby_gym/alternative.rb
+++ ruby_gym/alternative.rb
@@ -1,5 +1,68 @@
-number = 1 
-if number.odd? == true
-  then pp "odd"
-else pp "even"
+integers = [1, 21, 35, 105]
+integer = integers.sample
+# write your program below
+integer= 3125
+pp integer
+#3 5 7 all factors 
+if integer%3 == 0 
+  then 
+  if integer%5== 0 
+  then  
+  if integer%7== 0
+  then pp "PlingPlangPlong"
+  exit
+end
+end
+end
+
+
+
+#3 and 7
+if integer%3 == 0 
+  then 
+  if integer%7== 0
+  then pp "PlingPlong"
+  exit
+end
+end
+
+
+#3 and 5
+if integer%3 == 0 
+  then 
+  if integer%5== 0
+  then pp "PlingPlang"
+  exit
+end
+end
+
+#5 and 7
+if integer%5 == 0 
+  then 
+  if integer%7== 0
+  then pp "PlangPlong"
+  exit
+end
+end
+
+
+# 3 only 
+if integer%3 == 0
+  then pp "Pling"
+  exit
 end 
+
+# 5 only 
+if integer%5 == 0
+  then pp "Plang"
+  exit
+end 
+
+# 7 only 
+if integer%7 == 0
+  then pp "Plong"
+  exit
+end 
+
+#none
+pp integer
--- ruby_gym/practice_grounds.rb
+++ ruby_gym/practice_grounds.rb
@@ -1,33 +1,34 @@
-integers = [1, 21, 35, 105]
-integer = integers.sample
+strings = [
+  "here 12 plus 25",
+  "puzzle number 04 ",
+  " "
+]
+string = strings.sample
+pp string
 # write your program below
-pp integer
-if integer%3 == 0
-  then pp "Pling"
-      #3 yes+check for 5
-      if integer%5 == 0
-        pp "Plang"
-        #3 and 5 yes + check for 7
-        if integer%7 == 0
-          pp "Plong"
-        end
-      #3 yes 5 no check 7
-      else
-         if integer%7 == 0
-          pp "Plong"
-         end
-    #3 no 
-        else 
-          #3 no 5 yes check 7
-          if integer%5 == 0
-          pp "Plang"
-            #check 7
-            if integer%7 == 0
-              pp "Plong"
-             end
-            #3 no #5no check 7
-            else 
-              if integer%7 == 0
-                pp "Plong"
-               end
-              end
+
+# defines number of spaces and total characters 
+total_char=string.length
+
+# = display w/o spaces & for counting
+count_spaces_string= string.gsub(/\s/,"")
+length_wo_spaces=count_spaces_string.length
+count_spaces= length_wo_spaces.to_i
+total_char.to_i
+#number_of_spaces= total_char - number_of_dig
+pp "Total characters:  #{total_char}"
+pp "Length w/o spaces: #{count_spaces}"
+
+#number of digits 
+count_digits_string= string.gsub(/[0-9]/,"")
+count_digits=count_digits_string.length
+count_digits_int= count_digits.to_i
+number_of_dig= total_char-count_digits_int 
+pp number_of_dig
+
+#number_letters= total_char - number_of_dig
+#number_letters= number_letters.to_i
+
+#pp "Number of letters in the string: #{number_letters}"
+pp "Number of spaces in the string: #{number_of_spaces}"
+pp "Number of digits in the string is: #{number_of_dig}"
