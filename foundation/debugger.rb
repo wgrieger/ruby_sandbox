55089555c8b2463d23148fe04aa58d33ff5bec35
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-10 02:40:23 +0000

    in progress

--- foundation/debugger.rb
+++ foundation/debugger.rb
@@ -1,7 +1,33 @@
 require "pry-byebug"
-
-f= "your lucky number is "
-
-l = rand(100)
-
-pp f + l
+years = [
+  1700,
+  1940,
+  2038
+]
+year = years.sample
+# write your program below
+pp year
+#parent & step 1
+if 
+  byebug
+  year%4=0
+  #parent
+  then
+#step 2 
+byebug
+    if year%100=0
+      then 
+      #step3
+      byebug
+      if year%400=0
+          then pp "leap year"
+        else pp "no leap year (from step 3)"
+        end 
+    #step 2 
+      else 
+        pp "leap year (from step 2)"  
+      end 
+#parent
+  else 
+    pp "no leap year"
+  end 
--- ruby_gym/practice_grounds.rb
+++ ruby_gym/practice_grounds.rb
@@ -6,24 +6,25 @@ years = [
 year = years.sample
 # write your program below
 pp year
+#year.to_s
 #parent & step 1
 if 
-  year%4=0
+  year%4==0
   #parent
   then
 #step 2 
-    if year%100=0
+    if year%100==0
       then 
       #step3
-      if year%400=0
-          then pp "leap year"
-        else pp "no leap year (from step 3)"
+      if year%400==0
+          then pp "#{year} is a leap year!"
+        else pp "#{year} is not a leap year."
         end 
     #step 2 
       else 
-        pp "leap year (from step 2)"  
+        pp "#{year} is a leap year!"  
       end 
 #parent
   else 
-    pp "no leap year"
+    pp "#{year} is not a leap year."
   end 
